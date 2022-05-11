33155ab0f5969d20fcc0e3ddf7e5fc26b67f4ea8
Daniel <dbaigel@yext.com>
2022-05-11 13:58:49 -0400

    made changes to fix errors

--- component-testing/src/index.js
+++ component-testing/src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {AnswersHeadlessProvider} from "@yext/answers-headless-react";
+import { AnswersHeadlessProvider } from "@yext/answers-headless-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+ReactDOM.render(
+
   <AnswersHeadlessProvider
-    experienceKey = "test-experience"
-    apiKey = "9641a03ad3de221fb9c566b0c9148351"
-    locale = "en"
+    experienceKey="test-experience"
+    apiKey="9641a03ad3de221fb9c566b0c9148351"
+    locale="en"
     experienceVersion="PRODUCTION"
   >
+
     <App />
   </AnswersHeadlessProvider>
  
+  ,
+
+  document.getElementById('root')
 );
+
 
 
