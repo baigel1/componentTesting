419f760ff00422e2147de75ac16a64d6bb15a69a
Daniel <dbaigel@yext.com>
2022-05-11 12:40:35 -0400

    downgraded to react 17.0.2

--- component-testing/src/index.js
+++ component-testing/src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {AnswersHeadlessProvider} from "@yext/answers-headless-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AnswersHeadlessProvider
+    experienceKey = "test-experience"
+    apiKey = "9641a03ad3de221fb9c566b0c9148351"
+    locale = "en"
+    experienceVersion="PRODUCTION"
+  >
     <App />
-  </React.StrictMode>
+  </AnswersHeadlessProvider>
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ component-testing/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    './node_modules/@yext/answers-react-components/lib/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
